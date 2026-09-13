c93905fd9298b412232646217189ce540e3632dc
Saas-As <staszebrev@gmail.com>
2026-09-13 21:39:33 +0300

    Найдена и исправлена ошибка в 7 задании

--- 07_secret.py
+++ 07_secret.py
@@ -37,7 +37,7 @@ secret_message = [
 word1 = secret_message[0][3]
 word2 = secret_message[1][9:13]
 word3 = secret_message[2][5:15:2]
-word4 = secret_message[3][7:13][::-1] 
-word5 = secret_message[4][16:21][::-1] 
+word4 = secret_message[3][12:6:-1] 
+word5 = secret_message[4][20:15:-1] 
 
 print(f'{word1} {word2} {word3} {word4} {word5}')
